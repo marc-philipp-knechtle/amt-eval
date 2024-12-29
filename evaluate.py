f3a0f28e9da363dd0adb489824fc39157dc4ef35
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2024-12-29 18:42:37 +0100

    refactor into separate main()

--- evaluate.py
+++ evaluate.py
@@ -13,6 +13,7 @@ from data.dataset import SchubertWinterreiseDataset
 def evaluate_inference_dir(predictions_dir: str, dataset_name: str, dataset_group: str):
     dataset_groups: List[str] = dataset_group.split(',')
 
+    # todo replace this with a determine dataset function
     dataset = SchubertWinterreiseDataset()
 
     metrics = defaultdict(list)
@@ -20,21 +21,19 @@ def evaluate_inference_dir(predictions_dir: str, dataset_name: str, dataset_grou
     predictions_filepaths: List[str] = glob(os.path.join(predictions_dir, '*.mid'))
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('predictions_dir', type=str)
     parser.add_argument('dataset_name', nargs='?', default='default',
                         help='The dataset which the predictions are evaluated on.')
     parser.add_argument('dataset_group', nargs='?', default=None, help='Comma-separated dataset groups')
     parser.add_argument('--save-path', default=None)
-
     args: argparse.Namespace = parser.parse_args()
     dataset_name: str = parser.parse_args().dataset_name
     predictions_dir = args.predictions_dir
     """
     directory  where the predictions to be evaluated are saved
     """
-
     datetime_str: str = datetime.now().strftime('%y%m%d-%H%M')
     logging_filepath: str
     if args.save_path is None:
@@ -45,5 +44,8 @@ if __name__ == '__main__':
         os.makedirs(os.path.dirname(logging_filepath))
     # filemode=a -> append
     logging.basicConfig(filename=logging_filepath, filemode="a", level=logging.INFO)
-
     evaluate_inference_dir(predictions_dir, dataset_name, dataset_group=args.dataset_group)
+
+
+if __name__ == '__main__':
+    main()
