4007fa9551e1f4f514bce5cf6d3094153c868a89
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-02-02 12:18:31 +0100

    remove shape parameter from decoding

--- utils/decoding.py
+++ utils/decoding.py
@@ -2,87 +2,86 @@ from typing import Tuple, List
 
 import mir_eval.util
 import numpy as np
-import torch
+
+import constants
 
 
-def extract_notes(onsets, frames, velocity, onset_threshold=0.5, frame_threshold=0.5):
+# def extract_notes(onsets, frames, velocity, onset_threshold=0.5, frame_threshold=0.5):
+#     """
+#     Finds the note timings based on the onsets and frames information
+#
+#     Parameters
+#     ----------
+#     onsets: torch.FloatTensor, shape = [frames, bins]
+#     frames: torch.FloatTensor, shape = [frames, bins]
+#     velocity: torch.FloatTensor, shape = [frames, bins]
+#     onset_threshold: float
+#     frame_threshold: float
+#
+#     Returns
+#     -------
+#     pitches:    np.ndarray of bin_indices
+#                 shape: (<length>, 1)
+#                 To my understanding, these are the pitch values for each time index
+#     intervals:  np.ndarray of rows containing (onset_index, offset_index)
+#                 shape: (<length>, 2)
+#                 Start and end of each note
+#     velocities: np.ndarray of velocity vales
+#                 shape: (<length>, 1)
+#                 Velocity value for each time index
+#     """
+#     onsets = (onsets > onset_threshold).cpu().to(torch.uint8)
+#     frames = (frames > frame_threshold).cpu().to(torch.uint8)
+#     # torch.cat = concatenates tensors. Requirement: each tensor has the same shape!
+#     # onsets[:1, :] = first row, keeping all columns (=time bin 0 with all possible key values)
+#     # onsets[1:, :] - onsets[:-1, :] = subtracts each row of onsets from the next row, creating the difference
+#     # This is true if the current index detects an onset and the next index does not.
+#     onset_diff = torch.cat([onsets[:1, :], onsets[1:, :] - onsets[:-1, :]], dim=0) == 1
+#
+#     pitches = []
+#     intervals = []
+#     velocities = []
+#
+#     for nonzero in onset_diff.nonzero():
+#         frame = nonzero[0].item()
+#         pitch = nonzero[1].item()
+#
+#         onset = frame
+#         offset = frame
+#         velocity_samples = []
+#
+#         while onsets[offset, pitch].item() or frames[offset, pitch].item():
+#             if onsets[offset, pitch].item():
+#                 velocity_samples.append(velocity[offset, pitch].item())
+#             offset += 1
+#             if offset == onsets.shape[0]:
+#                 break
+#
+#         if offset > onset:
+#             pitches.append(pitch)
+#             intervals.append([onset, offset])
+#             velocities.append(np.mean(velocity_samples) if len(velocity_samples) > 0 else 0)
+#
+#     return np.array(pitches), np.array(intervals), np.array(velocities)
+
+
+def notes_to_frames(pitches_midi, intervals) -> Tuple[np.ndarray, List[np.ndarray]]:
     """
-    Finds the note timings based on the onsets and frames information
+    Takes lists specifying notes and intervals. Returns active pitches in each frame.
+    -> This is required for multipitch evaluation!!!
 
     Parameters
     ----------
-    onsets: torch.FloatTensor, shape = [frames, bins]
-    frames: torch.FloatTensor, shape = [frames, bins]
-    velocity: torch.FloatTensor, shape = [frames, bins]
-    onset_threshold: float
-    frame_threshold: float
-
-    Returns
-    -------
-    pitches:    np.ndarray of bin_indices
-                shape: (<length>, 1)
-                To my understanding, these are the pitch values for each time index
-    intervals:  np.ndarray of rows containing (onset_index, offset_index)
-                shape: (<length>, 2)
-                Start and end of each note
-    velocities: np.ndarray of velocity vales
-                shape: (<length>, 1)
-                Velocity value for each time index
-    """
-    onsets = (onsets > onset_threshold).cpu().to(torch.uint8)
-    frames = (frames > frame_threshold).cpu().to(torch.uint8)
-    # torch.cat = concatenates tensors. Requirement: each tensor has the same shape!
-    # onsets[:1, :] = first row, keeping all columns (=time bin 0 with all possible key values)
-    # onsets[1:, :] - onsets[:-1, :] = subtracts each row of onsets from the next row, creating the difference
-    # This is true if the current index detects an onset and the next index does not.
-    onset_diff = torch.cat([onsets[:1, :], onsets[1:, :] - onsets[:-1, :]], dim=0) == 1
-
-    pitches = []
-    intervals = []
-    velocities = []
-
-    for nonzero in onset_diff.nonzero():
-        frame = nonzero[0].item()
-        pitch = nonzero[1].item()
-
-        onset = frame
-        offset = frame
-        velocity_samples = []
-
-        while onsets[offset, pitch].item() or frames[offset, pitch].item():
-            if onsets[offset, pitch].item():
-                velocity_samples.append(velocity[offset, pitch].item())
-            offset += 1
-            if offset == onsets.shape[0]:
-                break
-
-        if offset > onset:
-            pitches.append(pitch)
-            intervals.append([onset, offset])
-            velocities.append(np.mean(velocity_samples) if len(velocity_samples) > 0 else 0)
-
-    return np.array(pitches), np.array(intervals), np.array(velocities)
-
-
-def notes_to_frames(pitches, intervals, shape) -> Tuple[np.ndarray, List[np.ndarray]]:
-    """
-    Takes lists specifying notes sequences and return
-
-    Parameters
-    ----------
-    pitches: list of midi pitches
+    pitches_midi: list of midi pitches
     intervals: list of [onset, offset] time for each pitch
-    shape: the shape of the original piano roll, [n_frames, n_bins]
 
     Returns
     -------
     time: np.ndarray containing the frame indices, shape(n_frames,1)
     freqs: list of np.ndarray, each containing the frequency bin indices, shape(n_frames,[list of frequencies])
     """
-    roll = np.zeros(tuple(shape))
-    for pitch, (onset, offset) in zip(pitches, intervals):
-        if pitch >= 88:
-            continue
+    roll = np.zeros((max(max(row) for row in intervals), constants.MAX_MIDI))
+    for pitch, (onset, offset) in zip(pitches_midi, intervals):
         roll[onset:offset, pitch] = 1
 
     time = np.arange(roll.shape[0])
@@ -90,7 +89,7 @@ def notes_to_frames(pitches, intervals, shape) -> Tuple[np.ndarray, List[np.ndar
     return time, freqs
 
 
-def note_to_multipitch_realtime(pitches: np.ndarray, intervals: np.ndarray, shape: Tuple,
+def note_to_multipitch_realtime(pitches: np.ndarray, intervals: np.ndarray,
                                 scaling_frame_to_real: float) -> Tuple[np.ndarray, List[np.ndarray]]:
     """
     Required parameters for mir_eval.multipitch:
@@ -104,13 +103,12 @@ def note_to_multipitch_realtime(pitches: np.ndarray, intervals: np.ndarray, shap
 
     :param pitches: ndarray of pitch values in midi format, shape(n,1)
     :param intervals: onset and offset time specified for each pitch value, shape(n,2)
-    :param shape: frametime shape, this is required because of the further processing
     :param scaling_frame_to_real: scaling value.
 
     :return: time_seconds, freqs
     """
 
-    time, active_midis = notes_to_frames(pitches, intervals, shape)
+    time, active_midis = notes_to_frames(pitches, intervals)
     time_seconds = time * scaling_frame_to_real
     freqs = [mir_eval.util.midi_to_hz(midis) for midis in active_midis]
     return time_seconds, freqs
