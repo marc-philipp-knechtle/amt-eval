19459507817cd5ee2c2cc94e8e8b4420dcb9cc7f
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-02-17 09:18:49 +0100

    add PhA test sets

--- data/dataset_determination.py
+++ data/dataset_determination.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from data.dataset import Bach10Dataset, ChoralSingingDataset, MusicNetDataset
+from data.dataset import Bach10Dataset, ChoralSingingDataset, MusicNetDataset, PhenicxAnechoicDataset
 
 
 def dir_contains_other_dirs(dir_path) -> bool:
@@ -11,7 +11,7 @@ dataset_definitions_trans_comparing_paper = {
     'B10': lambda: Bach10Dataset(groups=['07', '08', '09', '10']),
     'CSD': lambda: ChoralSingingDataset(groups=['Bruckner_LocusIste']),
     'MuN': lambda: MusicNetDataset(groups=['MuN-10-var-test']),
-    'PhA': lambda: Bach10Dataset(groups=['bruckner', 'mozart']),
+    'PhA': lambda: PhenicxAnechoicDataset(groups=['bruckner', 'mozart']),
     'RWC': lambda: Bach10Dataset(groups=['07', '08', '09', '10']),
     'SWD': lambda: Bach10Dataset(groups=['07', '08', '09', '10']),
     'Trios': lambda: Bach10Dataset(groups=['07', '08', '09', '10']),
