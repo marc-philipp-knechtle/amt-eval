fcc6d71c4a191a7aa568e4f7dd339ce28d0e61b8
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-04-06 18:08:36 +0200

    add building all datasets as a test

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Amt-Eval
+This repository is aimed as a implementation agnostic amt metrics calculation tool.
+
+## Initializtion of the repo
+Create the Environment, creates env `amt-eval`. 
+```shell
+conda env create -f environment.yaml
+```
+
+## Testing
+Running the tests:
+```shell
+pytest
+```
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,26 +1,26 @@
 import os
 
-import pytest
-
-from data.dataset import PhenicxAnechoicDataset
+from data.dataset import PhenicxAnechoicDataset, SchubertWinterreiseDataset, WagnerRingDataset, Bach10Dataset, \
+    RwcDataset, TriosDataset, ChoralSingingDataset, MusicNetDataset
 
 tests_root = os.path.abspath(os.path.dirname(__file__))
+"""
+used so that the directories etc. can be referenced without using root path 
+"""
+
+dataset_definitions_minimal = {
+    'SWD': lambda: SchubertWinterreiseDataset(path='dataset_fixtures/Schubert_Winterreise_Dataset_v2-1', groups=['HU33'], neither_split='test'),
+    'WRD': lambda: WagnerRingDataset(path='dataset_fixtures/WagnerRing_v0-1', groups=['Furtwangler1953']),
+    'B10': lambda: Bach10Dataset(path='dataset_fixtures/Bach10', groups=['07']),
+    'PhA': lambda: PhenicxAnechoicDataset(path='dataset_fixtures/PHENICX-Anechoic', groups= ['beethoven']),
+    'RWC': lambda: RwcDataset(groups=['rwc']),
+    'Trios': lambda: TriosDataset(groups=['brahms']),
+    'CSD': lambda: ChoralSingingDataset(groups=['Bruckner_LocusIste']),
+    'MuN': lambda: MusicNetDataset(groups=['MuN-10-var-test']),
+}
 
 
-def test_swd():
-    ...
-
-
-def test_wrd():
-    ...
-
-
-def test_bach10():
-    ...
-
-
-def test_phenicx_anechoic():
-    phenicx_anechoic_dataset = PhenicxAnechoicDataset(
-        path=os.path.join(tests_root, 'dataset_fixtures/PHENICX-Anechoic/'),
-        groups=['beethoven'])
-    assert phenicx_anechoic_dataset.groups == ['beethoven']
+def test_build_all_datasets():
+    for key, value in dataset_definitions_minimal.items():
+        dataset = value()
+        assert str(dataset) is not None # -> just load the dataset with a testing group
