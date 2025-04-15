4cf5d17042784e9a80a1d7b0bb08ad6ddcfd8df6
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-04-15 11:53:11 +0200

    refactor manual AP calculation

--- metrics/ap.py
+++ metrics/ap.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+import scipy.integrate as sc_integrate
 from sklearn.metrics import auc
 
 
@@ -10,17 +11,11 @@ def calc_ap_from_prec_recall_pairs(precision_recall_pairs: List[Tuple[float, flo
     assert ap >= 0
     return ap
 
-def calc_ap_from_prec_recall_pairs_manual(precision_recall_pairs: List[Tuple[float, float]]) -> float:
-    precision_recall_pairs = sorted(precision_recall_pairs)
-    total_precision_recall_area = 0
-    prev_recall = 0
-    prev_precision = 0
-    for precision, recall in precision_recall_pairs:
-        area_to_add = (precision - prev_precision) * max(recall, prev_recall)
-        assert area_to_add >= 0
-        total_precision_recall_area += area_to_add
-        prev_precision = precision
-        prev_recall = recall
 
-    assert total_precision_recall_area >= 0
-    return total_precision_recall_area
+def calc_ap_from_prec_recall_pairs_manual(precision_recall_pairs: List[Tuple[float, float]]) -> float:
+    precision_recall_pairs_sorted_precision = sorted(precision_recall_pairs, key=lambda pair: pair[0])
+    precision, recall = zip(*precision_recall_pairs_sorted_precision)
+    # Using recall because it converges to 0 when the threshold becomes 1
+    ap = sc_integrate.simpson(recall, precision)
+    assert ap >= 0
+    return ap
