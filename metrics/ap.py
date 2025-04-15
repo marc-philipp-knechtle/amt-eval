94d8ace684a618abdb97bde18e9e7389fc5f754a
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-04-15 12:09:51 +0200

    doc

--- metrics/ap.py
+++ metrics/ap.py
@@ -5,6 +5,14 @@ from sklearn.metrics import auc
 
 
 def calc_ap_from_prec_recall_pairs(precision_recall_pairs: List[Tuple[float, float]]) -> float:
+    """
+    Calculate AP with sklearn.metrics.auc (Area Under Curve)
+    This is an alternative method to sklearn.metrics.average_precision_score
+    We cannot use said method in some cases because we rely on multiple thresholds (e.g. onset thresh and frame thresh)
+    Args:
+        precision_recall_pairs: List of Tuples with (precision, recall) values.
+    Returns: Average Precision score, calculated with sklearn
+    """
     precision_recall_pairs_sorted_precision = sorted(precision_recall_pairs, key=lambda pair: pair[0])
     precision, recall = zip(*precision_recall_pairs_sorted_precision)
     ap = auc(precision, recall)
