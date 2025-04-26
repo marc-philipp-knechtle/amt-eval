16a23c3e483f9f0725685c451412c3948b36be42
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-04-26 12:56:28 +0200

    add doc

--- tests/metrics/test_ap.py
+++ tests/metrics/test_ap.py
@@ -1,4 +1,4 @@
-from metrics.ap import calc_ap_from_prec_recall_pairs, calc_ap_from_prec_recall_pairs_manual
+from metrics.ap import calc_ap_from_prec_recall_pairs
 
 
 def test_ap_zero():
@@ -7,7 +7,13 @@ def test_ap_zero():
 
 
 def test_ap_one():
-    prec_recall_values1 = [(1.0, 1.0), (1.0, 1.0)]
+    """
+    Assumption: perfect curve, ap = 1 prediction is like the gt
+    th = 0 -> all positives -> precision = 0, recall = 1
+    th = 1 -> only true positives -> precision = 1, recall = 1
+    """
+    # th: 1
+    prec_recall_values1 = [(0.0, 1.0), (1.0, 1.0)]
     assert calc_ap_from_prec_recall_pairs(prec_recall_values1) == 1
 
 
@@ -15,7 +21,7 @@ def test_ap_integral():
     # imaginary thresholds: [0, 1]
     prec_recall_values = [(0.0, 1.0), (1.0, 0.0)]
     thresholds = [0.0, 1.0]
-    ap = calc_ap_from_prec_recall_pairs(prec_recall_values, True, thresholds)
+    ap = calc_ap_from_prec_recall_pairs(prec_recall_values, False, thresholds)
 
     assert ap == 0.5
 
