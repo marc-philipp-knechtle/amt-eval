5cc8d04e4ddc74f84c65f1d26abad27c48438eb7
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-05-15 11:36:26 +0200

    update plot definition

--- visualizations/plots.py
+++ visualizations/plots.py
@@ -2,10 +2,12 @@ from typing import Dict, List, Any, Tuple
 
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib
 
 
 def plot_threshold_optimization(onset_values_for_diagram: Dict[float, List],
                                 frame_values_for_diagram: Dict[float, List]):
+    matplotlib.rcParams.update({'font.size': 20})
     thrs_onset = []
     thrs_values_onset = []
     thrs_values_frame = []
@@ -16,7 +18,7 @@ def plot_threshold_optimization(onset_values_for_diagram: Dict[float, List],
         thrs_values_frame.append(np.mean(frame_values_for_diagram[thr]))
         thrs_values_combi.append(np.mean([np.mean(values), np.mean(frame_values_for_diagram[thr])]))
 
-    fig, ax = plt.subplots(figsize=(8, 8))
+    fig, ax = plt.subplots(figsize=(11, 12))
     plt.plot(thrs_onset, thrs_values_onset, label='F1 Onset Perf for Threshold')
     plt.plot(thrs_onset, thrs_values_frame, label='F1 Frame Perf for Threshold')
     plt.plot(thrs_onset, thrs_values_combi, label='F1 Onset/Frame Perf for Threshold')
