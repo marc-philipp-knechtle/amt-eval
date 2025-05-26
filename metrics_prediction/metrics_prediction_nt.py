a9174bda7910af699ab13f6157c768af6e1ac72a
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2025-05-26 11:26:32 +0200

    add ap on onset level

--- metrics_prediction/metrics_prediction_nt.py
+++ metrics_prediction/metrics_prediction_nt.py
@@ -6,6 +6,57 @@ def compute_annotation_array_nooverlap_from_midi(midifile: str):
     ...
 
 
+def compute_onset_array_nooverlap(note_events, num_time_frames, fs, annot_type='pitch_class', shorten=1.0):
+    """
+    This tries to be the equivalent function of the same func. for frame
+
+    Args:
+        note_events:
+        num_time_frames:
+        fs:
+        annot_type:
+        shorten:
+
+    Returns:
+
+    """
+    array_length = num_time_frames
+
+    if annot_type == 'pitch_class':
+        array_height = 12
+    elif annot_type == 'pitch':
+        array_height = 128
+    elif annot_type == 'piano':
+        array_height = 88
+    elif annot_type == 'instruments':
+        array_height = 1
+    else:
+        assert False, ['annotation type ' + str(annot_type) + ' not valid!']
+
+    annot_array = np.zeros((array_height, array_length))
+
+    note_events_cp = note_events.copy()
+    note_events_cp[:, :2] = np.floor(note_events_cp[:, :2] * fs).astype(int)
+
+    for line_num in range(note_events_cp.shape[0]):
+        start_ind = int(note_events_cp[line_num, 0])
+
+        if annot_type == 'pitch_class':
+            pitch_ind = int(np.mod(note_events_cp[line_num, 2], 12))
+        elif annot_type == 'pitch':
+            pitch_ind = int(note_events_cp[line_num, 2])
+        elif annot_type == 'instruments':
+            pitch_ind = 0
+        elif annot_type == 'piano':
+            pitch_ind = int(note_events_cp[line_num, 2])
+        else:
+            raise RuntimeError(f'Unexpected annotation type: {annot_type}')
+
+        annot_array[pitch_ind, start_ind - 2:start_ind + 1] = 1
+
+    return annot_array
+
+
 def compute_annotation_array_nooverlap(note_events, num_time_frames, fs, annot_type='pitch_class', shorten=1.0):
     """ Converts a note event list into a binary np array, assuming a given frame rate
     From cweiss -> multipitch_architectures
